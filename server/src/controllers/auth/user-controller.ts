fca22e2c6f5601095e73dabe88a181aa567879cd
amnesia2k <tilewaolatoye17@gmail.com>
2025-06-02 12:08:27 +0100

    feat: Implement user registration and management features
    - Added user registration endpoint with validation and password hashing
    - Created user retrieval endpoints to fetch all users and individual user details
    - Introduced database connection setup using drizzle-orm with PostgreSQL
    - Defined user schema with validation using Zod
    - Implemented JWT token generation for user authentication
    - Added middleware for data validation
    - Set up Express server with dynamic route loading
    - Configured TypeScript for ESM support and strict type-checking

--- server/src/controllers/auth/user-controller.ts
+++ server/src/controllers/auth/user-controller.ts
@@ -75,3 +75,44 @@ export const registerUser = async (req: Request, res: Response) => {
     return;
   }
 };
+
+export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await db.select().from(usersTable);
+
+    // Remove password field from each user
+    const usersWithoutPassword = users.map(({ password, ...user }) => user);
+
+    res.status(200).json({
+      message: "All Users fetched successfully!",
+      users: usersWithoutPassword,
+    });
+    return;
+  } catch (error) {
+    console.error("Error fetching all users:", error);
+    res.status(500).json({ message: "Server error, try again later" });
+    return;
+  }
+};
+
+export const getUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const user = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable._id, id));
+
+    const userWithoutPassword = user.map(({ password, ...user }) => user);
+
+    res.status(200).json({
+      message: "User fetched successfully!",
+      user: userWithoutPassword[0],
+    });
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).json({ message: "Server error, try again later" });
+    return;
+  }
+};
--- server/src/routes/user-routes.ts
+++ server/src/routes/user-routes.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import { validateData } from "../middleware/validation-middleware";
 import { registerUserSchema } from "../db/schema-validator";
-import { registerUser } from "../controllers/auth/user-controller";
+import {
+  getAllUsers,
+  getUser,
+  registerUser,
+} from "../controllers/auth/user-controller";
 
 const router = express.Router();
 
 router.post("/register", validateData(registerUserSchema), registerUser);
+router.get("/get-users", getAllUsers);
+router.get("/user/:id", getUser);
 
 export default router;
